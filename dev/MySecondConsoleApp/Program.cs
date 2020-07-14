bd7a1a79cfc0b0576f4b55917157bcff90126865
user <bite.reinis@gmail.com>
2020-07-14 19:17:40 +0300

    20200714_19_17

---
+++ dev/MySecondConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MySecondConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		var txt = "EDIBO";
+        int num = 123;
+    		Console.WriteLine($"Hello World! {txt} RTU {num} ");
+        }
+    }
+}
---
+++ dev/MyWebApp/MansApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MansApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
