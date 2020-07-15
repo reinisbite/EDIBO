07bbdab2251cea7b988441ac142a3f0b84a3e80a
user <bite.reinis@gmail.com>
2020-07-15 17:13:53 +0300

    20200715_17_13

---
+++ Mans-Angular-App/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { XyzComponent } from './xyz/xyz.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    XyzComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatSliderModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+      
+
+    
