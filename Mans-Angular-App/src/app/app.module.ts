efaf404aeacdf03e95001ee719931aa0ca366cca
user <bite.reinis@gmail.com>
2020-07-16 17:18:55 +0300

    20200716_17_18

--- Mans-Angular-App/src/app/app.module.ts
+++ Mans-Angular-App/src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { XyzComponent } from './xyz/xyz.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { SlidersComponent } from './sliders/sliders.component'; 
 @NgModule({
   declarations: [
     AppComponent,
-    XyzComponent
+    XyzComponent,
+    SlidersComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatSliderModule,
     BrowserAnimationsModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
