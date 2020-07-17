45e269180b4b35595f6fb4b04ca84dd9f2f43d34
user <bite.reinis@gmail.com>
2020-07-17 17:19:17 +0300

    20200717_17_19

--- Mans-Angular-App/src/app/app.component.ts
+++ Mans-Angular-App/src/app/app.component.ts
@@ -9,3 +9,5 @@ export class AppComponent {
   title = 'Mans-Angular-App';
   imagesrc="https://images.pexels.com/photos/4597126/pexels-photo-4597126.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
 }
+
+
--- Mans-Angular-App/src/app/app.module.ts
+++ Mans-Angular-App/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { XyzComponent } from './xyz/xyz.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import { SlidersComponent } from './sliders/sliders.component'; 
+import { SlidersComponent } from './sliders/sliders.component';
+import { CheckboxComponent } from './checkbox/checkbox.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     XyzComponent,
-    SlidersComponent
+    SlidersComponent,
+    CheckboxComponent
   ],
   imports: [
     BrowserModule,
@@ -20,11 +24,11 @@ import { SlidersComponent } from './sliders/sliders.component';
     MatSliderModule,
     BrowserAnimationsModule,
     MatProgressSpinnerModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-      
 
-    
+
