af1a6002d4c1e5a654a897ca25846667b759ecbe
user <bite.reinis@gmail.com>
2020-07-28 19:05:21 +0300

    20200728_19_05

---
+++ JHipster/Variables.java
@@ -0,0 +1,18 @@
+
+public class Variables {
+        public static void main(String[] args) {
+                
+        byte b = 127;
+        short ss = 3200;
+        int x = 4;
+        long bigNumber = 100L;
+        float y = 3.14f;
+        double z = 2.72;
+        char c = 'A';
+        boolean decision = true;
+
+        System.out.println("The Byte primative = " +b);
+        System.out.println("The bigNumber = " + bigNumber);
+        System.out.println("Helo World!");
+        System.out.println("float = " + c);
+        }}
