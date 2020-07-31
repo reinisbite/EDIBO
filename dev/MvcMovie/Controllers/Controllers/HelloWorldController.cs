ba54402b6282c56a407d1afd0f8265ae4611c106
user <bite.reinis@gmail.com>
2020-07-31 18:34:13 +0300

    20200731_18_34

---
+++ dev/MvcMovie/Controllers/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        // public string Index()
+        // {
+        //     return "This is my default action...";
+        // }
+        public IActionResult Index()
+{
+    return View();
+}
+
+
+       
+
+  public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        } 
+    }
+}
